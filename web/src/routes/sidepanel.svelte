6e4effe31dda43ab2c8e94fbd5888d5efd4b6d8d
Jonas Lindberg <eksno@protonmail.com>
2024-09-03 01:39:07 +0700

    feat(sidepanel): team switcher and sidepanel improvements

--- web/src/routes/sidepanel.svelte
+++ web/src/routes/sidepanel.svelte
@@ -13,31 +13,32 @@
 </script>
 
 <div
+	role="navigation"
 	on:mouseenter={() => (isCollapsed = false)}
 	on:mouseleave={() =>
 		setTimeout(() => {
 			isCollapsed = true;
 		}, 150)}
-	class="flex min-w-14 max-w-14 flex-col overflow-hidden text-nowrap transition-all duration-200 ease-in-out hover:min-w-52 hover:max-w-52"
+	class="group flex min-w-14 max-w-14 flex-col overflow-hidden text-nowrap transition-all duration-200 ease-in-out hover:min-w-52 hover:max-w-52"
 >
-	<div class={cn('flex h-14 flex-col items-center justify-center px-2')}>
+	<div class={cn('flex h-14 flex-col items-center justify-center px-1')}>
 		<TeamSwitcher {isCollapsed} {teams} />
 	</div>
 	<Separator class="mt-0 pt-0" />
 	<Nav {isCollapsed} routes={primaryRoutes} />
 	<Separator />
-	<div class="grid gap-1 px-2">
+	<div class="relative grid h-8">
 		<Typography
 			align="left"
 			variant="body-lg"
-			class={cn('self-center px-4 pt-2', isCollapsed ? 'block' : 'hidden')}
+			class="absolute bottom-0 left-0 right-0 top-0 h-full px-[1.45rem] py-2"
 		>
 			T
 		</Typography>
 		<Typography
 			align="left"
 			variant="body-lg"
-			class={cn('px-4 pt-2', isCollapsed ? 'hidden' : 'block')}
+			class="absolute bottom-0 left-0 right-0 top-0 h-full px-[1.45rem] py-2 opacity-0 transition-all duration-200 ease-in-out group-hover:opacity-100"
 		>
 			Team Management
 		</Typography>
@@ -45,18 +46,18 @@
 	<Nav {isCollapsed} routes={teamRoutes} />
 	<Separator />
 
-	<div class="grid gap-1 px-2">
+	<div class="relative grid h-8">
 		<Typography
 			align="left"
 			variant="body-lg"
-			class={cn('px-3.5 pt-2', isCollapsed ? 'block' : 'hidden')}
+			class="absolute bottom-0 left-0 right-0 top-0 h-full px-[1.45rem] py-2"
 		>
 			P
 		</Typography>
 		<Typography
 			align="left"
 			variant="body-lg"
-			class={cn('px-3.5 pt-2', isCollapsed ? 'hidden' : 'block')}
+			class="absolute bottom-0 left-0 right-0 top-0 h-full px-[1.45rem] py-2 opacity-0 transition-all duration-200 ease-in-out group-hover:opacity-100"
 		>
 			Profile Management
 		</Typography>
--- web/src/routes/team-switcher.svelte
+++ web/src/routes/team-switcher.svelte
@@ -3,6 +3,7 @@
 	import { Typography } from '$lib/components/ui/typography';
 	import type { Team } from '$lib/supabase';
 	import { Icon } from '$lib/icons';
+	import { ChevronDown } from 'lucide-svelte';
 
 	export let isCollapsed: boolean;
 	export let teams: Team[];
@@ -24,28 +25,31 @@
 			selectedTeam = teams.find((team) => team.name === e?.value) || teams[0];
 		}}
 	>
-		<Select.Trigger class="relative grid" aria-label="Select team">
+		<Select.Trigger class="relative grid h-12 border-primary/0" aria-label="Select team">
 			<div
-				class="absolute bottom-0 left-0 right-0 top-0 ml-2 flex items-center justify-start"
+				class="absolute bottom-0 left-0 right-0 top-0 ml-0.5 flex items-center justify-start"
 			>
-				<Icon src={selectedTeam.icon_url} class="mr-4 size-6 min-h-6 min-w-6" />
-				<Typography variant="title-sm">{selectedTeam.name}</Typography>
+				<Icon src={selectedTeam.icon_url} class="mr-3 size-11 min-h-11 min-w-11" />
+				<div class="flex w-full items-center justify-between pr-2">
+					<Typography variant="title-md">{selectedTeam.name}</Typography>
+					<ChevronDown class="opacity-50" />
+				</div>
 			</div>
 		</Select.Trigger>
 		<Select.Content sameWidth={!isCollapsed} align={isCollapsed ? 'center' : undefined}>
 			<Select.Group>
 				{#each teams as team}
-					<Select.Item value={team.name} label={team.name}>
-						<div
-							class="flex items-center justify-start gap-3 [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 [&_svg]:text-foreground"
-						>
-							<Icon
-								src={team.icon_url}
-								aria-hidden="true"
-								class="size-6 min-h-6 min-w-6 text-foreground"
-							/>
-							<Typography variant="title-sm">{team.name}</Typography>
-						</div>
+					<Select.Item
+						class="flex items-center justify-start gap-2"
+						value={team.name}
+						label={team.name}
+					>
+						<Icon
+							src={team.icon_url}
+							aria-hidden="true"
+							class="size-8 min-h-8 min-w-8"
+						/>
+						<Typography variant="title-sm">{team.name}</Typography>
 					</Select.Item>
 				{/each}
 			</Select.Group>
