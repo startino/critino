b17fe4c04acb183988a69cf3b72694c4dc533479
Jonas Lindberg <eksno@protonmail.com>
2024-09-02 18:13:29 +0700

    feat(project): breadcrumb nav

--- web/src/routes/projects/[project]/+page.svelte
+++ web/src/routes/projects/[project]/+page.svelte
@@ -2,6 +2,7 @@
 	import * as Resizable from '$lib/components/ui/resizable';
 	import { Separator } from '$lib/components/ui/select';
 	import { Typography } from '$lib/components/ui/typography';
+	import Breadcrumb from '../../breadcrumb.svelte';
 
 	export let data;
 
@@ -10,13 +11,13 @@
 
 <Resizable.Pane>
 	<div class="flex h-14 items-center px-4">
-		<Typography as="h1" variant="body-lg" class="flex items-center justify-center">
-			{team.name}
-			<span class="px-4 text-2xl opacity-30">/</span>
-			projects
-			<span class="px-4 text-2xl opacity-30">/</span>
-			{project.name}
-		</Typography>
+		<Breadcrumb
+			crumbs={[
+				{ name: team.name, href: '/' },
+				{ name: 'projects', href: '/projects' },
+				{ name: project.name, href: `/projects/${project.id}` },
+			]}
+		/>
 	</div>
 	<Separator />
 	{JSON.stringify(project, null, 2)}
