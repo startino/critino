fa3c3358804923219b3a9b6f2a4a14c176c1f68e
Jonas Lindberg <eksno@protonmail.com>
2024-09-04 02:01:48 +0700

    feat(api): debugging

--- web/src/routes/api/[team]/[project]/[agent]/+server.ts
+++ web/src/routes/api/[team]/[project]/[agent]/+server.ts
@@ -1,9 +1,9 @@
 export const POST = async ({ request }) => {
-	// 	const data = await request.json();
-	//
-	// 	console.log(JSON.stringify(data, null, 2));
-	//
-	return new Response(JSON.stringify({ message: 'Hello' }), {
+	const data = await request.json();
+
+	console.log(JSON.stringify(data, null, 2));
+
+	return new Response(JSON.stringify({ message: 'Hello', data }), {
 		headers: { 'Content-Type': 'application/json' },
 	});
 };
