ecb09a7c32bc15a2ca7e8752c7234edf4fbf3ffb
Jonas Lindberg <eksno@protonmail.com>
2024-09-04 02:14:10 +0700

    feat(api): creating a critique

--- web/src/routes/api/[team]/[project]/[agent]/+server.ts
+++ web/src/routes/api/[team]/[project]/[agent]/+server.ts
@@ -1,9 +1,27 @@
-export const POST = async ({ request }) => {
+export const POST = async ({ params, request, locals: { supabase } }) => {
 	const data = await request.json();
 
-	console.log(JSON.stringify(data, null, 2));
+	const { data: critique, error: eCritique } = await supabase
+		.from('critiques')
+		.insert({
+			team_name: params.team,
+			project_name: params.project,
+			agent_name: params.agent,
+			...data,
+		})
+		.select()
+		.single();
+	if (!critique || eCritique) {
+		const message = `Error creating critique: ${JSON.stringify(eCritique, null, 2)}`;
+		console.error(message);
+		return new Response(JSON.stringify({ status: 500, message }), {
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
 
-	return new Response(JSON.stringify({ message: 'Hello', data }), {
+	console.log(JSON.stringify(critique, null, 2));
+
+	return new Response(JSON.stringify({ status: 200, message: 'Success', critique }), {
 		headers: { 'Content-Type': 'application/json' },
 	});
 };
