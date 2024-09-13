57d7432f80892d3c888e3377ae798a6c7ba0f13a
Jonas Lindberg <eksno@protonmail.com>
2024-09-13 11:39:44 +0700

    fix(config): max duration set to 300

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -19,7 +19,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			maxDuration: 300, // Set the timeout to 300 seconds
+		}),
 		alias: {
 			$styling: './src/app.pcss',
 		},
