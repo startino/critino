de64728dad072abf3b4a516914787378152f6d39
Jonas Lindberg <eksno@protonmail.com>
2024-09-14 12:47:07 +0000

    feat(api): start command

--- services/api/src/__init__.py
+++ services/api/src/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import uvicorn
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
@@ -23,3 +26,27 @@ app.add_middleware(
 @app.get("/")
 def redirect_to_docts():
     return RedirectResponse(url="/docs")
+
+
+def run():
+    # app = create_app()
+    # sandbox_app = sandbox.create_app()
+    # app.mount("/sandbox", sandbox_app)
+
+    try:
+        PORT = os.environ.get("PORT")
+        if not PORT:
+            logging.info(
+                "PORT not found in environment variables. Using default PORT=8000"
+            )
+            PORT = 8000
+
+        PORT = int(PORT)
+    except Exception:
+        raise ValueError("PORT is not an integer")
+
+    uvicorn.run(app, host="0.0.0.0", port=PORT, log_config="logging.yaml")
+
+
+if __name__ == "__main__":
+    run()
