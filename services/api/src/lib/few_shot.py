163bdd8d6155928b21a1afd6d267f0edde9302f9
Jonas Lindberg <eksno@protonmail.com>
2024-09-18 17:33:19 +0000

    feat(routes): upsert critique

--- services/api/src/lib/few_shot.py
+++ services/api/src/lib/few_shot.py
@@ -19,49 +19,44 @@ class StrippedCritique(BaseModel):
     optimal: str
 
 
-def format_critiques(critiques: list[StrippedCritique]) -> list[dict]:
-    return [
-        {
-            "context": critique.context,
-            "query": critique.query,
-            "optimal": critique.optimal,
-        }
-        for critique in critiques
-    ]
-
-
-def few_shot_example_messages(
-    critiques: list[StrippedCritique], query: str, k: int = 4
-) -> dict:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    if OPENAI_API_KEY is None:
-        logging.error("OPENAI_API_KEY is not set")
-        raise ValueError("OPENAI_API_KEY is not set")
-    OPENAI_API_KEY = SecretStr(OPENAI_API_KEY)
-
-    examples = format_critiques(critiques)
-    embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
-
-    example_selector = SemanticSimilarityExampleSelector.from_examples(
-        examples,
-        embeddings,
-        InMemoryVectorStore,
-        k=k,
-        input_keys=["query"],
-    )
-
-    examples = example_selector.select_examples({"query": query})
-
+def format_example_string(critiques: list[StrippedCritique]) -> str:
     example_prompt = PromptTemplate.from_template(
         "<example><context>{context}</context><query>{query}</query><output>{optimal}</output></example>"
     )
 
     few_shot = FewShotPromptTemplate(
-        examples=examples,
+        examples=[critique.model_dump() for critique in critiques],
         example_prompt=example_prompt,
         example_separator="",
         prefix="<examples>",
         suffix="</examples>",
     ).format()
 
-    return {"string": few_shot, "examples": examples}
+    return xml_utils.trim_xml(few_shot)
+
+
+def find_relevant_critiques(
+    critiques: list[StrippedCritique], query: str, k: int = 4
+) -> list[StrippedCritique]:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    if OPENAI_API_KEY is None:
+        logging.error("OPENAI_API_KEY is not set")
+        raise ValueError("OPENAI_API_KEY is not set")
+    OPENAI_API_KEY = SecretStr(OPENAI_API_KEY)
+
+    embeddings = OpenAIEmbeddings(api_key=OPENAI_API_KEY)
+    print("1")
+
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        [critique.model_dump() for critique in critiques],
+        embeddings,
+        InMemoryVectorStore,
+        k=k,
+        input_keys=["query"],
+    )
+    print("2")
+
+    return [
+        StrippedCritique(**critique)
+        for critique in example_selector.select_examples({"query": query})
+    ]
--- services/api/src/routers/index.py
+++ services/api/src/routers/index.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel
+from pydantic import UUID4, BaseModel
 from src.interfaces import db
 from src.lib.few_shot import (
-    few_shot_example_messages,
+    format_example_string,
+    find_relevant_critiques,
     StrippedCritique,
 )
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import RedirectResponse
 
 router = APIRouter()
@@ -16,17 +17,53 @@ def redirect_to_docs() -> RedirectResponse:
     return RedirectResponse(url="/docs")
 
 
-@router.get("/critique-ids")
+@router.get("/critiques/ids")
 def get_critique_ids() -> list[str]:
     supabase = db.client()
     response = supabase.table("critiques").select("id").execute()
     return [critique["id"] for critique in response.data]
 
 
-@router.get("/get_examples")
-def get_examples() -> dict:
+class GetCritiquesRelevantRequest(BaseModel):
+    query: str
+    k: int = 4
+    team_name: str
+    project_name: str
+    workflow_name: str | None = None
+    agent_name: str | None = None
+
+
+class GetCritiquesRelevantResult(BaseModel):
+    critiques: list[StrippedCritique]
+    examples: str
+
+
+@router.get("/critiques/relevant")
+def get_relevant_critiques(
+    q: GetCritiquesRelevantRequest = Depends(),
+) -> GetCritiquesRelevantResult:
+    if (q.agent_name is not None) and (q.workflow_name is None):
+        raise HTTPException(
+            status_code=400,
+            detail="If you provide an agent_name, you must also provide a workflow_name",
+        )
+
     supabase = db.client()
-    response = supabase.table("critiques").select("*").execute()
+    request = (
+        supabase.table("critiques")
+        .select("*")
+        .eq("team_name", q.team_name)
+        .eq("project_name", q.project_name)
+    )
+
+    if q.workflow_name is not None:
+        request.eq("workflow_name", q.workflow_name)
+
+    if q.agent_name is not None:
+        request.eq("agent_name", q.agent_name)
+
+    response = request.execute()
+
     critiques = [
         StrippedCritique(
             optimal=critique["optimal"],
@@ -36,6 +73,52 @@ def get_examples() -> dict:
         for critique in response.data
     ]
 
-    query = "This is the query"
+    relevant_critiques = find_relevant_critiques(critiques, q.query, k=q.k)
 
-    return few_shot_example_messages(critiques, query, k=4)
+    return GetCritiquesRelevantResult(
+        critiques=relevant_critiques, examples=format_example_string(relevant_critiques)
+    )
+
+
+class PostCritiquesRequest(BaseModel):
+    id: str
+    context: str | None = None
+    query: str | None = None
+    optimal: str | None = None
+    response: str | None = None
+    team_name: str
+    project_name: str
+    workflow_name: str
+    agent_name: str
+
+
+@router.post("/critiques")
+def upsert_a_critique(q: PostCritiquesRequest) -> None:
+    supabase = db.client()
+
+    (
+        supabase.table("workflows")
+        .upsert(
+            {
+                "team_name": q.team_name,
+                "project_name": q.project_name,
+                "name": q.workflow_name,
+            }
+        )
+        .execute()
+    )
+
+    (
+        supabase.table("agents")
+        .upsert(
+            {
+                "team_name": q.team_name,
+                "project_name": q.project_name,
+                "workflow_name": q.workflow_name,
+                "name": q.agent_name,
+            }
+        )
+        .execute()
+    )
+
+    supabase.table("critiques").upsert(q.model_dump()).execute()
