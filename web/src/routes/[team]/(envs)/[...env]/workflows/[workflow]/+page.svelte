f4fe2d4c396f9f7a35bf732de49c76712249c51d
Jonas Lindberg <eksno@protonmail.com>
2024-10-11 01:31:43 +0700

    feat(critiques): only show agents with critiques

--- web/src/routes/[team]/(envs)/[...env]/workflows/[workflow]/+page.svelte
+++ web/src/routes/[team]/(envs)/[...env]/workflows/[workflow]/+page.svelte
@@ -2,23 +2,27 @@
 	import * as Tabs from '$lib/components/ui/tabs';
 	import CritiqueTable from './critique-table.svelte';
 	import { Typography } from '$lib/components/ui/typography';
+	import type { Tables } from '$lib/supabase';
 	export let data;
 
 	$: ({ supabase, team, environment, workflow, agents, critiques } = data);
+
+	const filterAgents = (agents: Tables<'agents'>[]) =>
+		agents.filter((agent) => critiques.some((critique) => critique.agent_name === agent.name));
 </script>
 
 <div class="flex w-full items-center justify-center p-12">
-	{#if agents.length > 1}
+	{#if filterAgents(agents).length > 1}
 		<Tabs.Root
-			value={(agents.find((agent) => agent.name) ?? { name: '' }).name}
+			value={(filterAgents(agents).find((agent) => agent.name) ?? { name: '' }).name}
 			class="h-full w-full"
 		>
 			<Tabs.List class="flex w-full gap-2">
-				{#each agents as agent}
+				{#each filterAgents(agents) as agent}
 					<Tabs.Trigger class="flex-1" value={agent.name}>{agent.name}</Tabs.Trigger>
 				{/each}
 			</Tabs.List>
-			{#each agents as agent}
+			{#each filterAgents(agents) as agent}
 				<Tabs.Content value={agent.name}>
 					<CritiqueTable
 						agent={agent.name}
@@ -30,8 +34,14 @@
 				</Tabs.Content>
 			{/each}
 		</Tabs.Root>
-	{:else if agents[0]}
-		<CritiqueTable agent={agents[0].name} {supabase} {environment} {workflow} {critiques} />
+	{:else if filterAgents(agents)[0]}
+		<CritiqueTable
+			agent={filterAgents(agents)[0]!.name}
+			{supabase}
+			{environment}
+			{workflow}
+			{critiques}
+		/>
 	{:else}
 		<div class="mx-auto h-full w-full text-left">
 			<Typography variant="display-lg">No Critiques Created Yet</Typography>
