6ac86797f2bbc5c1e08137fcf0bffe0dbb4dbeb4
Muhammad_Usman <usmanijazz444@gmail.com>
2025-02-03 01:28:24 +0500

    Updated the Prompt to handle the User Defined Definations correctly and output the required results. Updated the document loader to handle online text files

--- services/api/src/lib/critique_generator.py
+++ services/api/src/lib/critique_generator.py
@@ -1,7 +1,7 @@
 import os
 from pydantic import BaseModel
 from typing import List
-from langchain_community.document_loaders import YoutubeLoader, TextLoader, PyPDFLoader, Docx2txtLoader
+from langchain_community.document_loaders import YoutubeLoader, TextLoader, PyPDFLoader, Docx2txtLoader,UnstructuredURLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
@@ -33,7 +33,7 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20
 
 def process_youtube(url: str) -> List[str]:
     try:
-        yt_loader = YoutubeLoader.from_youtube_url(url, add_video_info=False)
+        yt_loader = YoutubeLoader.from_youtube_url(url, add_video_info=False,language = ["en", "en-US", "en-GB", "en-CA", "en-AU"])
         transcript = yt_loader.load()
         return text_splitter.split_text(transcript[0].page_content)
     except Exception as e:
@@ -41,15 +41,17 @@ def process_youtube(url: str) -> List[str]:
 
 def process_documents(input_type: str, source: str) -> List[str]:
     try:
-        if input_type == "pdf":
-            loader = PyPDFLoader(source)
-        elif input_type == "txt":
-            loader = TextLoader(source)
-        elif input_type == "docx":
-            loader = Docx2txtLoader(source)
-        else:
-            raise HTTPException(status_code=400, detail="Unsupported document type")
-
+        # if input_type == "pdf":
+        #     loader = PyPDFLoader(source)
+        # elif input_type == "txt":
+        #     loader = TextLoader(source)
+        # elif input_type == "docx":
+        #     loader = Docx2txtLoader(source)
+        # else:
+        #     raise HTTPException(status_code=400, detail="Unsupported document type")
+        
+        #Loading content from online Source File
+        loader = UnstructuredURLLoader(urls=[source])
         docs = loader.load()
 
         return text_splitter.split_text("\n".join([doc.page_content for doc in docs]))
@@ -76,31 +78,30 @@ def generate_critique(request: CritiqueGeneratorRequest) -> List[CritiqueGenerat
     for chunk in text_chunks:
         prompt = ChatPromptTemplate([
             SystemMessage(
-                content="""
-                You are an expert AI critique assistant. Your task is to analyze the given content and provide detailed and constructive critiques in a specific Critino Format based on the user-defined definitions. 
-                Your critiques should be insightful, actionable, and aimed at improving the quality of the content.
+                content=f"""
+                You are an expert AI critique assistant. Your task is to analyze the given content and provide detailed and constructive critiques in a specific Critino Format. 
+                You need to analyze the content and populate the fields/attributes in JSON according to the User-Defined Definations.
+
+                ## User-Defined Definitions ##
+                -context: {request.context}\n
+                -query: {request.query}\n
+                -optimal_response: {request.optimal_response}\n
 
                 Each Critique output should follow the following Critino Format as a Valid JSON object
-                - context: The prior discussion or information that helps in understanding the topic or question being addressed. It sets the stage for the query.
-                - query: The exact question, statement, or request that led to a response. It represents the main point that needs an answer.
-                - optimal_response: The best possible reply that is precise, relevant, and well-suited to the query. It serves as the standard for evaluating other responses.
+                - context
+                - query
+                - optimal_response
                 - situation: A brief, generalized description (around 10 words) summarizing the core theme of the context and query, making it useful for identifying similar cases.
                 
-                Only include the above mentioned Critino Format attributes in the JSON. DO NOT ADD ANY OTHER EXTRA ATTIRIBUTES.
+                Only include the above mentioned Critino Format attributes/fields in the JSON. DO NOT ADD ANY OTHER EXTRA ATTIRIBUTES.
                 """
             ),
             HumanMessage(
                 content=f"""
                 
-                ## User-Defined Definitions ##
-                -context: {request.context}\n
-                -query: {request.query}\n
-                -optimal_response: {request.optimal_response}\n
-                
                 Text Chunk: {chunk}\n
 
-                Based on the provided context, query, and optimal response, analyze the text chunk and provide a detailed critique. 
-                Ensure your critique is clear, concise, and actionable.
+                Analyze the text chunk and provide a detailed critique.
                 """
             )
         ])
