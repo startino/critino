6059d0fd3fd72e2336e84b733e9c5539b7b6c65c
Jonas Lindberg <eksno@protonmail.com>
2025-06-03 18:35:56 +0700

    feat(api): use service role key to bypass rls

--- services/api/src/interfaces/db.py
+++ services/api/src/interfaces/db.py
@@ -8,10 +8,10 @@ def client() -> Client:
     logging.debug("Creating Supabase client")
     load_dotenv()
     url: str | None = os.environ.get("PUBLIC_SUPABASE_URL")
-    key: str | None = os.environ.get("PUBLIC_SUPABASE_ANON_KEY")
+    key: str | None = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 
     if url is None or key is None:
-        logging.error("PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_ANON_KEY must be set")
-        raise ValueError("PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_ANON_KEY must be set")
+        logging.error("PUBLIC_SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY must be set")
+        raise ValueError("PUBLIC_SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY must be set")
 
     return create_client(url, key)
